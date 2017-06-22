78fd4ea43daa18d170137fbb3129f0a535b52bfe
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-22 16:35:03 +0900

    initial implementation

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ansible-role-ssh
 
-A brief description of the role goes here.
+Configures `/etc/ssh/ssh_known_hosts` and `/etc/ssh/ssh_config`. This role does
+not manage users' `known_hosts` or `ssh_config`.
 
 # Requirements
 
@@ -8,9 +9,24 @@ None
 
 # Role Variables
 
-| variable | description | default |
+| Variable | Description | Default |
 |----------|-------------|---------|
+| `ssh_conf_dir` | path to directory for system's `ssh` configuration files | `/etc/ssh` |
+| `ssh_known_hosts_file` | path to `ssh_known_hosts(5)` | `{{ ssh_conf_dir }}/ssh_known_hosts` |
+| `ssh_known_hosts` | list of dict of public key (see below) | `[]` |
+| `ssh_config_file` | path to `ssh_config(5)` | `{{ ssh_conf_dir }}/ssh_config` |
+| `ssh_config` | content of `ssh_config` | `""` |
 
+## `ssh_known_hosts`
+
+This variable is a list of dict. See [`known_hosts`
+module](http://docs.ansible.com/ansible/known_hosts_module.html) for details.
+
+| Key | Value | Description |
+|-----|-------|-------------|
+| `name` | String | host or IP address of the key |
+| `state` | `present` or `absent` | Remove all keys that belong to `name` if `absent`. Add the key to `ssh_known_hosts` if `present` |
+| `key` | String | the key |
 
 # Dependencies
 
@@ -19,6 +35,24 @@ None
 # Example Playbook
 
 ```yaml
+- hosts: localhost
+  roles:
+    - ansible-role-ssh
+  vars:
+    ssh_config: |
+      Port 22
+      Protocol 2
+    ssh_known_hosts:
+      - name: anoncvs.ca.openbsd.org
+        key: "anoncvs.ca.openbsd.org ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCz6RLtgGksBp/0dH7M5vGCUxgD31+wX28tnLlij90+cYhjELDV3HX95DypEA7xfIN6W8Vg/GOJkX4Oot+zpQXNQx3VeOyMgcn4KXO83XYGsPVfJQijjzyI0r0/ztEsxYAE6JHEiEvY9floDnNRyoFLVETNE5oB9yBcDIt6W6BYjlpXqJNsEPy7ij+kBbEk7QT0FcyFidp7FmExsOQy23nhQ55A/6fB7ATsDQtz+snniF9ZJg5+b71SYzxfhUPkxJhmhBkx7NmPnRjy7eE0I7qrHODrHONIi1LWCo0joTIAfVgxhEn5SDbviTAINAecGgis5LQqXp0xSupfWuozZeXV"
+        state: present
+      - name: anoncvs.au.openbsd.org
+        # or lookup()
+        key: "{{ lookup('file', 'files/anoncvs.au.openbsd.org.pub') }}"
+        state: present
+      - name: github.com
+        key: "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
+        state: present
 ```
 
 # License
--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,58 +1,49 @@
 require "spec_helper"
 require "serverspec"
 
-package = "ssh"
-service = "ssh"
-config  = "/etc/ssh/ssh.conf"
-user    = "ssh"
-group   = "ssh"
-ports   = [PORTS]
-log_dir = "/var/log/ssh"
-db_dir  = "/var/lib/ssh"
+config_dir = "/etc/ssh"
+ssh_config = "#{config_dir}/ssh_config"
+ssh_known_hosts = "#{config_dir}/ssh_known_hosts"
+keys = [
+  "anoncvs.au.openbsd.org ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKDCcpTR+00ygfH+7WnZGWUKWrDvH1gaXkBeyGaVkPLf73mIJEuaCwh0aWalf+ccc76VWj6CRLnC6W3Mj/9VK7U=",
+  "anoncvs.ca.openbsd.org ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCz6RLtgGksBp/0dH7M5vGCUxgD31+wX28tnLlij90+cYhjELDV3HX95DypEA7xfIN6W8Vg/GOJkX4Oot+zpQXNQx3VeOyMgcn4KXO83XYGsPVfJQijjzyI0r0/ztEsxYAE6JHEiEvY9floDnNRyoFLVETNE5oB9yBcDIt6W6BYjlpXqJNsEPy7ij+kBbEk7QT0FcyFidp7FmExsOQy23nhQ55A/6fB7ATsDQtz+snniF9ZJg5+b71SYzxfhUPkxJhmhBkx7NmPnRjy7eE0I7qrHODrHONIi1LWCo0joTIAfVgxhEn5SDbviTAINAecGgis5LQqXp0xSupfWuozZeXV",
+  "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
+]
+default_user = "root"
+default_group = "root"
 
 case os[:family]
 when "freebsd"
-  config = "/usr/local/etc/ssh.conf"
-  db_dir = "/var/db/ssh"
+  default_group = "wheel"
+when "openbsd"
+  default_group = "wheel"
 end
 
-describe package(package) do
-  it { should be_installed }
-end
-
-describe file(config) do
+describe file(ssh_config) do
+  it { should exist }
   it { should be_file }
-  its(:content) { should match Regexp.escape("ssh") }
+  it { should be_mode 644 }
+  it { should be_owned_by default_user }
+  it { should be_grouped_into default_group }
 end
 
-describe file(log_dir) do
+describe file(ssh_known_hosts) do
   it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/ssh") do
-    it { should be_file }
+  it { should be_file }
+  it { should be_mode 644 }
+  it { should be_owned_by default_user }
+  it { should be_grouped_into default_group }
+  keys.each do |k|
+    its(:content) { should match(/^#{Regexp.escape(k)}$/) }
   end
 end
 
-describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
-end
-
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
-  end
+describe file(ssh_config) do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 644 }
+  it { should be_owned_by default_user }
+  it { should be_grouped_into default_group }
+  its(:content) { should match(/^Port 22$/) }
+  its(:content) { should match(/^Protocol 2$/) }
 end
